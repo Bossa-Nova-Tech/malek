a4ceaaae6f1c7725bacfa8020d09620d9b82be23
Rafa Borges <93266793+okborges@users.noreply.github.com>
2022-07-26 10:35:09 -0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-container>
+    <h1>main</h1>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+
+  head() {
+    return {
+      bodyAttrs: {
+        class: 'home',
+      },
+
+      title: `${process.env.title}`,
+
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: '',
+        },
+      ],
+    };
+  },
+};
+</script>
