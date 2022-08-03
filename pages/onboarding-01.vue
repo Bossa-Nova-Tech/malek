ad80216635b660623ca5af9b9004a2250bdeb1d3
Rafael Borges <rafaelborges.av@gmail.com>
2022-08-03 11:31:29 -0300

    finalizando telas onboarding

--- pages/onboarding-01.vue
+++ pages/onboarding-01.vue
@@ -1,53 +1,83 @@
 <template>
-  <main>
-    <Flicking :options="{ align: 'prev' }">
-      <div class="flicking-panel">
+  <main class="caixa mx-auto">
+    <Flicking :options="{ align: 'prev' }" :plugins="plugins">
+      <div class="pb-5 width-page">
         <h1 class="text-center malek-x">Seja bem vindo</h1>
         <div class="d-flex justify-content-between mx-auto cont-img">
-          <img src="~/assets/img/ilustracao/blue clipboard.svg" alt="" />
+          <img src="~/assets/img/ilustracao/clipboard.png" alt="" />
           <img
             class="left"
-            src="~/assets/img/ilustracao/cup of coffee right.svg"
+            src="~/assets/img/ilustracao/copo-cafe-direita.png"
             alt=""
           />
         </div>
-        <img
-          class="d-flex mx-auto"
-          src="~/assets/img/ilustracao/dots-01.svg"
-          alt=""
-        />
         <p class="mx-auto">
           Aqui você vai poder criar e enviar propostas comerciais, organizar a
           sua agenda e distribuir e acompanhar tarefas para a sua equipe
         </p>
       </div>
 
-      <div class="flicking-panel">
+      <div class="width-page">
         <h1 class="text-center malek-x">Gestão de vendas</h1>
         <div class="d-flex justify-content-between mx-auto cont-img">
-          <img src="~/assets/img/ilustracao/cup of coffee left.svg" alt="" />
-          <img src="~/assets/img/ilustracao/Tablet-right.svg" alt="" />
+          <img src="~/assets/img/ilustracao/copo-cafe-esquerda.png" alt="" />
+          <img src="~/assets/img/ilustracao/tablet-direita.png" alt="" />
         </div>
-        <img
-          class="d-flex mx-auto"
-          src="~/assets/img/ilustracao/dots-02.svg"
-          alt=""
-        />
         <p class="mx-auto">
           Crie com poucos cliques propostas comerciais com a identidade visual
           da sua empresa
         </p>
       </div>
+
+      <div class="width-page">
+        <h1 id="gerenciamento" class="text-center malek-x">
+          Gerenciamento <br />de tarefas
+        </h1>
+        <div class="d-flex justify-content-between mx-auto cont-img">
+          <img
+            class="tablet-left"
+            src="~/assets/img/ilustracao/tablet-esquerda.png"
+            alt=""
+          />
+          <img id="caneta" src="~/assets/img/ilustracao/caneta.png" alt="" />
+        </div>
+        <p class="mx-auto">
+          Organize a agenda da semana sua e dos profissionais da sua equipe,
+          para garantir mais produtividade
+        </p>
+      </div>
+
+      <div class="pb-5 width-page">
+        <h1 class="text-center malek-x">Financeiro</h1>
+        <div class="d-flex justify-content-between mx-auto cont-img">
+          <img
+            class="mx-auto"
+            src="~/assets/img/ilustracao/contas.png"
+            alt=""
+          />
+        </div>
+        <p class="mx-auto">
+          Organize contas a pagar e a receber em um só lugar
+        </p>
+      </div>
+      <div slot="viewport" class="flicking-pagination"></div>
     </Flicking>
   </main>
 </template>
 
 <script>
 import { Flicking } from '@egjs/vue-flicking';
+import { Pagination } from '@egjs/flicking-plugins';
+import '@egjs/flicking-plugins/dist/pagination.css';
 
 export default {
   components: {
     Flicking,
+  },
+  data() {
+    return {
+      plugins: [new Pagination({ type: 'bullet' })],
+    };
   },
 };
 </script>
@@ -69,14 +99,30 @@ p {
   padding-top: 3.75rem;
 }
 
+span {
+  color: aqua;
+}
+
 .cont-img {
-  position: relative;
-  max-width: 700px;
   max-height: 350px;
   padding-bottom: 2.5rem;
 }
 
-.cont-img img {
+.width-page {
   width: 100%;
+}
+</style>
+
+<style>
+.flicking-pagination-bullet-active {
+  background-color: #0047bb;
+}
+
+#gerenciamento {
+  padding-bottom: 2.1rem;
+}
+
+#caneta {
+  object-fit: contain;
 }
 </style>
